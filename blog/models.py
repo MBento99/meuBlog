0997a8885f618d231ec446f7ddee7c40dfcb18e9
Magno <magnobento92@gmail.com>
2019-06-05 21:33:59 -0300

    alteração dia 05/06/2019

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('index/',views.index, name='index'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
+    path('/comment', views.comment_list, name='comment_list'),
 ]
